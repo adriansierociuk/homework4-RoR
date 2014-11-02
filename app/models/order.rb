cc12dedeb5e2665778950809e90290e1737c5baa
Aswebmaster <adriansierociuk@gmail.com>
2014-11-02 17:08:39 +0100

    make

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ActiveRecord::Base
   validates(:product, presence:true)
   belongs_to :customer
-  accepts_nested_attributes_for :customer
+  accepts_nested_attributes_for :customer, :reject_if => :no_name
 
-
+  def no_name(attributes)
+    attributes[:name].blank?
+  end
 end
