fabc6c9038e4763fbb54d033902ed02ae644c4ed
Aswebmaster <adriansierociuk@gmail.com>
2014-11-02 16:33:16 +0100

    make

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ActiveRecord::Base
- # validates(:city, presence: true)
+  validates(:city, presence: true)
  # validates(:number, numerilacity: {greather_than_or_equal_to: 1})
  # validates(:street, presence: true)
 end
