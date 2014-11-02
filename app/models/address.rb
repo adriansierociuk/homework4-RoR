2b48212a13000021125cb9a3aa7201544330b883
Aswebmaster <adriansierociuk@gmail.com>
2014-11-02 16:36:53 +0100

    make

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,5 @@
 class Address < ActiveRecord::Base
   validates(:city, presence: true)
   validates(:street, presence: true)
-  validates(:number, numerilacity: {greater_than_or_equal_to: 1})
-
+  validates(:number, numericality: {greater_than_or_equal_to: 1})
 end
